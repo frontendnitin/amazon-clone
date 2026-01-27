f5e92002d0579e40da5bae4311a8a9e6a37189a3
Nitin Chauhan <nitinchauhan707864@gmail.com>
2026-01-27 23:50:34 +0530

    Updated sponsoredSection

--- amazon-clone/src/Screen/HomeScreen/SponsoredSection/sponsoredSection.jsx
+++ amazon-clone/src/Screen/HomeScreen/SponsoredSection/sponsoredSection.jsx
@@ -7,29 +7,38 @@ const SponsoredSection = () => {
   const { product } = sponsoredData;
   return (
     <div className="sponsoredSection">
-      <div className="sponsoredCard">
-        <div className="sponsoredImg">
-          <img src={product.img} alt={product.name} />
-        </div>
-        <div className="sponsoredDetails">
-          <div className="sponsoredTitle">{product.name}</div>
-          <div className="sponsoredRating">
-            <RatingStars rating={product.rating} count={product.ratingCount}/>
+      <div className="sponsoredWrapper">
+        <div className="sponsoredCard">
+          <div className="sponsoredImg">
+            <img src={product.img} alt={product.name} />
           </div>
-          <div className="priceRow">
-            <span className="newPrice">₹{product.price}</span>
-            <span className="oldPrice">₹{product.oldPrice}</span>
-            {product.isPrime && (
-              <span className="primeBadge">
-                <FaCheckCircle className="primeIcon" />
-                <span className="primeText">prime</span>
-              </span>
-            )}
+          <div className="sponsoredDetails">
+            <div className="sponsoredTitle">{product.name}</div>
+            <div className="sponsoredRating">
+              <RatingStars
+                rating={product.rating}
+                count={product.ratingCount}
+              />
+            </div>
+            <div className="priceRow">
+              <div className="newPrice">
+                <span className="currency">₹</span>
+                <span className="amount">{product.price}</span>
+                <span className="fraction">00</span>
+              </div>
+              <span className="oldPrice">₹{product.oldPrice}</span>
+              {product.isPrime && (
+                <span className="primeBadge">
+                  <FaCheckCircle className="primeIcon" />
+                  <span className="primeText">prime</span>
+                </span>
+              )}
+            </div>
+            <button className="shopBtn">Shop now</button>
           </div>
-          <button className="shopBtn">Shop now</button>
         </div>
+        <div className="sponsoredText">Sponsored i</div>
       </div>
-      <div className="sponsoredText">Sponsored i</div>
     </div>
   );
 };
