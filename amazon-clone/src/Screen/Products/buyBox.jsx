8a12fc65830f5be1c280b45403281aad934b15fa
Nitin Chauhan <nitinchauhan707864@gmail.com>
2026-04-23 22:32:52 +0530

    added dynamic quantity feature in buyBox.jsx

--- amazon-clone/src/Screen/Products/buyBox.jsx
+++ amazon-clone/src/Screen/Products/buyBox.jsx
@@ -1,9 +1,121 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./buyBox.css";
+import { MdDone } from "react-icons/md";
+const BuyBox = ({ product }) => {
+  const [quantity, setQuantity] = useState(1);
+  const [open, setOpen] = useState(false);
 
-const buyBox = () => {
+  // const quantities = [1, 2, 3, 4, 5];
+
   return (
-    <div>buyBox</div>
-  )
-}
+    <div className="buyBoxContainer">
+      <div className="productNewPrice">
+        <span className="itemProductcurrency">₹</span>
+        <span className="itemProductnewPrice">
+          {product.newprice.toLocaleString("en-IN")}
+        </span>
+      </div>
 
-export default buyBox
+      <span className="primeBadge">
+        <MdDone className="primeIcon" />
+        <span className="primeText">prime</span>
+      </span>
+
+      <div className="taxText">Inclusive of all taxes</div>
+
+      {/* Delivery */}
+      <div className="deliverySection">
+        FREE delivery <b>Monday, 18 March</b>
+        <div className="deliverAddress">Deliver to Nitin - 110084</div>
+      </div>
+
+      {/* Stock */}
+      <div className="stockText">In Stock</div>
+
+      {/* seller info */}
+      <div className="sellerInfo">
+        <div className="infoRow">
+          <span className="infoLabel">Ships from</span>
+          <span className="infoValue">Amazon</span>
+        </div>
+
+        <div className="infoRow">
+          <span className="infoLabel">Sold by</span>
+          <span className="infoValue sellerName">{product.seller}</span>
+        </div>
+
+        <div className="infoRow">
+          <span className="infoLabel">Gift options</span>
+          <span className="infoValue">Available at checkout</span>
+        </div>
+
+        <div className="infoRow">
+          <span className="infoLabel">Payment</span>
+          <span className="infoValue">Secure transaction</span>
+        </div>
+      </div>
+
+      {/* <div className="quantityBox" onClick={() => setOpen(!open)}>
+
+        <span>Quantity: {quantity}</span>
+        <span className="dropdownArrow">▼</span>
+
+      </div>
+
+      {open && (
+
+        <div className="quantityDropdown">
+
+          {quantities.map((q) => (
+            <div
+              key={q}
+              className="quantityOption"
+              onClick={() => {
+
+                setQuantity(q)
+                setOpen(false)
+
+              }}
+            >
+              {q}
+            </div>
+          ))}
+
+        </div>
+
+      )} */}
+
+      <div className="quantityWrapper">
+        <div className="quantityBox" onClick={() => setOpen(!open)}>
+          <span>Quantity: {quantity}</span>
+          <span>▼</span>
+        </div>
+
+        {open && (
+          <div className="quantityDropdown">
+            {Array.from({ length: 25 }, (_, i) => i + 1).map((q) => (
+              <div
+                key={q}
+                className="quantityOption"
+                onClick={() => {
+                  setQuantity(q);
+                  setOpen(false);
+                }}
+              >
+                {q}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+      {/* Buttons */}
+      <div className="buyButtons">
+        <button className="addCartBtn">Add to Cart</button>
+        <button className="buyNowBtn">Buy Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default BuyBox;
--- amazon-clone/src/Screen/Products/productInfo.jsx
+++ amazon-clone/src/Screen/Products/productInfo.jsx
@@ -4,13 +4,24 @@ import { MdDone } from "react-icons/md";
 import RatingStars from "../../Component/RatingStars/RatingStars"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPercent } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRotateLeft, faTruckFast, faShieldHalved , faHandHoldingDollar,faTrophy, faLock } from '@fortawesome/free-solid-svg-icons';
+
+const iconMap = {
+  replace: faArrowRotateLeft,
+  Delivery: faTruckFast,
+  secure: faShieldHalved,
+  dollar:faHandHoldingDollar ,
+  brand:faTrophy,
+  lock:faLock
+};
+
 const productInfo = ({ product }) => {
   return (
     <div className="productInfoPage">
       <div className="topDesc">
         <div className='product-title'>{product.name}</div>
         <span className='visit-product'>Visit the Store</span>
-        <div className="productInfo">
+        <div className="productRatingRow">
           <div className="productRating">
             <div className="ratingNo">{product.ratingcount}</div>
             <RatingStars
@@ -19,7 +30,7 @@ const productInfo = ({ product }) => {
             />
           </div>
           <div className="border-margin">|</div>
-          <span className='pageSearch'>Search this Page</span>
+          <span className='pageSearch'>Search this page</span>
         </div>
       </div>
       <hr />
@@ -32,7 +43,7 @@ const productInfo = ({ product }) => {
           <span className="oldPrice">M.R.P: ₹{product.oldprice.toLocaleString("en-IN")}</span>
         </div>
         <span className="primeBadge">
-          <MdDone className="primeIcon" />
+          <MdDone className="primeIcon"/>
           <span className="primeText">prime</span>
         </span>
         <span className='texesText'>Inclusive of all taxes</span>
@@ -42,7 +53,7 @@ const productInfo = ({ product }) => {
             <span className='offerText'>Offers</span>
           </div>
           <ul className="offerDetails">
-            {product.offer.map((offers, index) => ( 
+            {product.offer.map((offers, index) => (
               // <span className='offerList'> . {offers}</span>
               <li className='offerList' key={index}>
                 <span className='offerListText'>{offers}</span>
@@ -50,6 +61,28 @@ const productInfo = ({ product }) => {
               </li>
             ))}
           </ul>
+        </div>
+        <div className="serviceSection">
+          {product.services.map((service, index) => (
+
+            <div className="serviceDetails" key={index}>
+              <FontAwesomeIcon icon={iconMap[service.icon]} className="serviceIcons" />
+              <span className='service-Text'>{service.text}</span>
+            </div>
+          ))}
+        </div>
+        <hr />
+        <div className="bottomDesc">
+            <div className="aboutProduct">
+              <h3 className='aboutTitle'>About this item</h3>
+              <ul className='aboutList'>
+                {product.about.map((point,index)=>(
+                  <li key={index} className='aboutItem'>
+                    {point}
+                  </li>
+                ))}
+              </ul>
+            </div>
         </div>
       </div>
     </div>
